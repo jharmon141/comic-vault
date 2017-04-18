8b1720347ccf65afba2ee9cf0854a5745da10693
jharmon141 <jeremy.l.harmon@gmail.com>
2017-04-18 18:28:34 -0500

    :robot:

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+
+    state: {
+    },
+
+    mutations: {
+    },
+
+    actions: {
+    },
+
+    getters: {
+    }
+
+})
+
+export default store
