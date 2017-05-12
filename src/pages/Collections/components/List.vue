81eea6fe87e7539ad0f52d918e8b6123a465c8fa
jharmon141 <jeremy.l.harmon@gmail.com>
2017-05-11 21:36:50 -0500

    :rocket:

--- src/pages/Collections/components/List.vue
+++ src/pages/Collections/components/List.vue
@@ -84,8 +84,8 @@ export default {
                 }).then((data) => {
                     window.localStorage.setItem("Snackbar", true)
                     window.localStorage.setItem("snackMessage", "Comic removed from collection")
+                    this.$router.push({ path: '/' })
                     location.reload()
-                    this.$router.push({ path: '/' });
                 }).catch((error) => {
                     console.error(error)
                 })
